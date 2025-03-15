c45f5de1ca13125555266e9363645237965d900a
Ogu1208 <kdasunb6@gmail.com>
2025-03-15 15:02:47 +0900

    feat: AccountController - 계좌 생성 컨트롤러 및 계좌 생성 jpa 메서드 구현

--- section2/accounts/src/main/java/com/easybytes/accounts/controller/AccountsController.java
+++ section2/accounts/src/main/java/com/easybytes/accounts/controller/AccountsController.java
@@ -1,13 +1,29 @@
 package com.easybytes.accounts.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.easybytes.accounts.constant.AccountsConstants;
+import com.easybytes.accounts.dto.CustomerDto;
+import com.easybytes.accounts.dto.ResponseDto;
+import com.easybytes.accounts.service.IAccountService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(path = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
+@AllArgsConstructor
 public class AccountsController {
 
-    @GetMapping("/sayHello")
-    public String sayHello() {
-        return "Hi World! hey~";
+    private IAccountService accountService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+
+        accountService.createAccount(customerDto);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 }
--- section2/accounts/src/main/java/com/easybytes/accounts/repository/CustomerRepository.java
+++ section2/accounts/src/main/java/com/easybytes/accounts/repository/CustomerRepository.java
@@ -4,6 +4,11 @@ import com.easybytes.accounts.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByMobileNumber(String mobileNumber);
 }
