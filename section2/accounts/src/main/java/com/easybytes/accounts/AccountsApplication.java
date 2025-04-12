44e99bdd95d9e73cde6fb45b0ec43a80a1ec514c
Ogu1208 <kdasunb6@gmail.com>
2025-04-12 13:28:56 +0900

    feat: Spring Data JPA의 auditing 설정 및 자동 설정 기능 추가

--- section2/accounts/src/main/java/com/easybytes/accounts/AccountsApplication.java
+++ section2/accounts/src/main/java/com/easybytes/accounts/AccountsApplication.java
@@ -2,12 +2,13 @@ package com.easybytes.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-public class    AccountsApplication {
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+public class AccountsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AccountsApplication.class, args);
     }
-
 }
